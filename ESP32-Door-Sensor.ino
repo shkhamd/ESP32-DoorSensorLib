c13eaa2cd828a3b88724a3babc8823509d474881
Shaikh Ahmed <shkhamd@users.noreply.github.com>
2018-09-27 03:32:00 +0600

    first working example commit of doorsensor lib

---
+++ ESP32-Door-Sensor.ino
@@ -0,0 +1,33 @@
+#include "doorsensor.h"
+
+#define SERIAL_BAUD       115200
+
+/*************************************
+ * Door sensor and interrupt
+ */
+DoorSensor DS(DOOR_SENSOR_PIN);
+void IRAM_ATTR DoorSensorInterrupt() 
+{
+  portENTER_CRITICAL_ISR(&mux);
+  DS.updateDoorState();
+  portEXIT_CRITICAL_ISR(&mux);
+}
+//====================================
+
+
+
+void setup() 
+{
+  Serial.begin(SERIAL_BAUD);
+  Serial.println("Monitoring Door: ");
+  attachInterrupt(digitalPinToInterrupt(DOOR_SENSOR_PIN), DoorSensorInterrupt, CHANGE);
+}
+ 
+void loop() 
+{
+   portENTER_CRITICAL(&DSmux);
+   if(DS.isDoorStateChanged())
+     Serial.println( DS.isDoorOpen() ? "DOOR OPEN" : "DOOR CLOSED" );
+   portEXIT_CRITICAL(&DSmux);
+   delay(1000);
+}
---
+++ doorsensor.h
@@ -0,0 +1,65 @@
+
+#define DOOR_SENSOR_PIN       12
+
+portMUX_TYPE DSmux = portMUX_INITIALIZER_UNLOCKED;
+
+class DoorSensor {
+  private:
+      byte _sensorPin;
+      volatile byte _sensorValue;
+      volatile byte _sensorChanged;
+  
+  public:
+      DoorSensor(const int sensorPin); //done
+      byte isDoorOpen(void);                //done
+      void updateDoorState(void);              //done
+      byte isDoorStateChanged(void);
+};
+
+/**
+ * intialize pins and variables
+ */
+DoorSensor::DoorSensor(const int sensorPin)
+{
+  pinMode(sensorPin, INPUT_PULLUP);
+  this->_sensorPin    =  sensorPin;
+  this->_sensorValue   =  digitalRead(this->_sensorPin);
+  this->_sensorChanged  =  1;
+}
+
+/*
+ * return the actual pin state of door sensor
+ * user called
+ */
+byte DoorSensor::isDoorOpen(void) { return this->_sensorValue; }
+
+/**
+ * updates the door sensor value
+ * called from inteuupt, keep short
+ */
+void DoorSensor::updateDoorState(void) 
+{ 
+  this->_sensorValue = digitalRead(this->_sensorPin); 
+  this->_sensorChanged = 1;
+}
+
+/**
+ * return if door is opened or closed
+ * user called
+ */
+byte DoorSensor::isDoorStateChanged(void) 
+{
+  if(this->_sensorChanged) 
+  {
+    this->_sensorChanged = 0;
+    return 1;
+  }
+  else return 0;
+}
+
+/*****************************************
+ ************ Interrupt & Global Code***
+ *****************************************/
+
+
+
